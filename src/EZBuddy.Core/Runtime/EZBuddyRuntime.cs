e3c20912e47014ec298515b9f0348b96e48b4324
Newbieisez <erezhaimowicz@gmail.com>
2026-09-06 15:04:49 -0700

    Add shared EZBuddy runtime services

---
+++ src/EZBuddy.Core/Runtime/EZBuddyRuntime.cs
@@ -0,0 +1,13 @@
+using EZBuddy.Core.Adapters;
+using EZBuddy.Core.Engine;
+
+namespace EZBuddy.Core.Runtime;
+
+public static class EZBuddyRuntime
+{
+    private static readonly Lazy<ActivityQueueEngine> QueueLazy = new(() => new ActivityQueueEngine());
+    private static readonly Lazy<AdapterRegistry> AdapterRegistryLazy = new(() => new AdapterRegistry());
+
+    public static ActivityQueueEngine Queue => QueueLazy.Value;
+    public static AdapterRegistry Adapters => AdapterRegistryLazy.Value;
+}
