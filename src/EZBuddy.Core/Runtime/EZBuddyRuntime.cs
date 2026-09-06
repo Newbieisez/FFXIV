225d6f2017578c7cca62ced5907adb29c373f95b
Newbieisez <erezhaimowicz@gmail.com>
2026-09-06 15:28:17 -0700

    Wire runtime license execution guard

--- src/EZBuddy.Core/Runtime/EZBuddyRuntime.cs
+++ src/EZBuddy.Core/Runtime/EZBuddyRuntime.cs
@@ -1,11 +1,13 @@
 using EZBuddy.Core.Adapters;
 using EZBuddy.Core.Engine;
+using EZBuddy.Core.Licensing;
 
 namespace EZBuddy.Core.Runtime;
 
 public static class EZBuddyRuntime
 {
-    private static readonly Lazy<ActivityQueueEngine> QueueLazy = new(() => new ActivityQueueEngine());
+    private static readonly Lazy<ActivityQueueEngine> QueueLazy = new(() =>
+        new ActivityQueueEngine(executionGate: LicenseExecutionGuard.Instance));
     private static readonly Lazy<AdapterRegistry> AdapterRegistryLazy = new(() => new AdapterRegistry());
 
     public static ActivityQueueEngine Queue => QueueLazy.Value;
