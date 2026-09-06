21c4d0515cf712e7b67201ecb80190d4c0128366
Newbieisez <erezhaimowicz@gmail.com>
2026-09-06 15:39:21 -0700

    Expose shared retainer bell coordinator

--- src/EZBuddy.Core/Runtime/EZBuddyRuntime.cs
+++ src/EZBuddy.Core/Runtime/EZBuddyRuntime.cs
@@ -1,6 +1,7 @@
 using EZBuddy.Core.Adapters;
 using EZBuddy.Core.Engine;
 using EZBuddy.Core.Licensing;
+using EZBuddy.Core.Retainers;
 
 namespace EZBuddy.Core.Runtime;
 
@@ -9,7 +10,9 @@ public static class EZBuddyRuntime
     private static readonly Lazy<ActivityQueueEngine> QueueLazy = new(() =>
         new ActivityQueueEngine(executionGate: LicenseExecutionGuard.Instance));
     private static readonly Lazy<AdapterRegistry> AdapterRegistryLazy = new(() => new AdapterRegistry());
+    private static readonly Lazy<RetainerBellCoordinator> RetainerBellCoordinatorLazy = new(() => new RetainerBellCoordinator());
 
     public static ActivityQueueEngine Queue => QueueLazy.Value;
     public static AdapterRegistry Adapters => AdapterRegistryLazy.Value;
+    public static RetainerBellCoordinator RetainerBellCoordinator => RetainerBellCoordinatorLazy.Value;
 }
