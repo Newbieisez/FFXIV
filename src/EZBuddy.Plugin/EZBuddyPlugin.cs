e06a4e0975236fc2b24ec7050fb99b8e18f2970a
Newbieisez <erezhaimowicz@gmail.com>
2026-09-06 16:55:02 -0700

    Register Grand Company refill integration

--- src/EZBuddy.Plugin/EZBuddyPlugin.cs
+++ src/EZBuddy.Plugin/EZBuddyPlugin.cs
@@ -130,6 +130,7 @@ public sealed class EZBuddyPlugin : BotPlugin
         EZBuddyRuntime.Adapters.Register(new OrderBotAdapter());
         EZBuddyRuntime.Adapters.Register(new LisbethAdapter());
         EZBuddyRuntime.Adapters.Register(new LlamaRetainerSweepAdapter());
+        EZBuddyRuntime.Adapters.Register(new LlamaGrandCompanyAdapter());
     }
 
     private void CloseDashboard()
