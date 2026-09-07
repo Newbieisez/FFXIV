ef747de6aab50cbd3ad8d54bb82650f19e174658
Newbieisez <erezhaimowicz@gmail.com>
2026-09-06 17:04:02 -0700

    Expose shared runtime telemetry hub

--- src/EZBuddy.Core/Runtime/EZBuddyRuntime.cs
+++ src/EZBuddy.Core/Runtime/EZBuddyRuntime.cs
@@ -7,12 +7,14 @@ namespace EZBuddy.Core.Runtime;
 
 public static class EZBuddyRuntime
 {
+    private static readonly Lazy<ActivityTelemetryHub> TelemetryLazy = new(() => new ActivityTelemetryHub());
     private static readonly Lazy<ActivityQueueEngine> QueueLazy = new(() =>
-        new ActivityQueueEngine(executionGate: LicenseExecutionGuard.Instance));
+        new ActivityQueueEngine(TelemetryLazy.Value, LicenseExecutionGuard.Instance));
     private static readonly Lazy<AdapterRegistry> AdapterRegistryLazy = new(() => new AdapterRegistry());
     private static readonly Lazy<RetainerBellCoordinator> RetainerBellCoordinatorLazy = new(() => new RetainerBellCoordinator());
 
     public static ActivityQueueEngine Queue => QueueLazy.Value;
+    public static ActivityTelemetryHub Telemetry => TelemetryLazy.Value;
     public static AdapterRegistry Adapters => AdapterRegistryLazy.Value;
     public static RetainerBellCoordinator RetainerBellCoordinator => RetainerBellCoordinatorLazy.Value;
 }
