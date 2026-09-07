20aa5d710be3cb565e801adab63b817e36db3c41
Newbieisez <erezhaimowicz@gmail.com>
2026-09-06 17:12:03 -0700

    Add live duty leveling progress provider

---
+++ src/EZBuddy.RebornBuddy/Duties/RebornBuddyDutyLevelingProgressProvider.cs
@@ -0,0 +1,15 @@
+using EZBuddy.Core.Duties;
+using ff14bot.Managers;
+
+namespace EZBuddy.RebornBuddy.Duties;
+
+public sealed class RebornBuddyDutyLevelingProgressProvider : IDutyLevelingProgressProvider
+{
+    public DutyLevelingProgress Read()
+    {
+        var player = ff14bot.Core.Player;
+        return new DutyLevelingProgress(
+            CurrentLevel: player?.ClassLevel ?? 0,
+            FreeInventorySlots: InventoryManager.FreeSlots);
+    }
+}
