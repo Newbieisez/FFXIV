e53dc71c8f7e2c8caa8c96817c123c8acaea05de
Newbieisez <erezhaimowicz@gmail.com>
2026-09-06 17:06:41 -0700

    Register Duty Support adapter

--- src/EZBuddy.Plugin/EZBuddyPlugin.cs
+++ src/EZBuddy.Plugin/EZBuddyPlugin.cs
@@ -179,6 +179,7 @@ public sealed class EZBuddyPlugin : BotPlugin
         EZBuddyRuntime.Adapters.Register(new MagitekAdapter());
         EZBuddyRuntime.Adapters.Register(new OrderBotAdapter());
         EZBuddyRuntime.Adapters.Register(new LisbethAdapter());
+        EZBuddyRuntime.Adapters.Register(new RebornBuddyDutySupportAdapter());
         EZBuddyRuntime.Adapters.Register(new LlamaRetainerSweepAdapter());
         EZBuddyRuntime.Adapters.Register(new LlamaGrandCompanyAdapter());
     }
