b07b8df8e860630345819b62349e7b9d8c8ed672
Newbieisez <erezhaimowicz@gmail.com>
2026-09-06 17:37:40 -0700

    Add first loop view codebehind

---
+++ src/EZBuddy.UI/Views/FirstPlayableLoopView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace EZBuddy.UI.Views;
+
+public partial class FirstPlayableLoopView : UserControl
+{
+    public FirstPlayableLoopView()
+    {
+        InitializeComponent();
+    }
+}
