a9166f3b9ad036abf75dbbf0ee6f5d0601c4e788
Newbieisez <erezhaimowicz@gmail.com>
2026-09-06 23:32:44 -0700

    Expose shared notification hub from runtime

--- src/EZBuddy.Core/Runtime/EZBuddyRuntime.cs
+++ src/EZBuddy.Core/Runtime/EZBuddyRuntime.cs
@@ -1,6 +1,7 @@
 using EZBuddy.Core.Adapters;
 using EZBuddy.Core.Engine;
 using EZBuddy.Core.Licensing;
+using EZBuddy.Core.Notifications;
 using EZBuddy.Core.Retainers;
 
 namespace EZBuddy.Core.Runtime;
@@ -8,6 +9,7 @@ namespace EZBuddy.Core.Runtime;
 public static class EZBuddyRuntime
 {
     private static readonly Lazy<ActivityTelemetryHub> TelemetryLazy = new(() => new ActivityTelemetryHub());
+    private static readonly Lazy<NotificationHub> NotificationsLazy = new(() => new NotificationHub());
     private static readonly Lazy<ActivityQueueEngine> QueueLazy = new(() =>
         new ActivityQueueEngine(TelemetryLazy.Value, LicenseExecutionGuard.Instance));
     private static readonly Lazy<RunLoopController> RunLoopLazy = new(() => new RunLoopController(QueueLazy.Value));
@@ -17,6 +19,7 @@ public static class EZBuddyRuntime
     public static ActivityQueueEngine Queue => QueueLazy.Value;
     public static RunLoopController RunLoop => RunLoopLazy.Value;
     public static ActivityTelemetryHub Telemetry => TelemetryLazy.Value;
+    public static NotificationHub Notifications => NotificationsLazy.Value;
     public static AdapterRegistry Adapters => AdapterRegistryLazy.Value;
     public static RetainerBellCoordinator RetainerBellCoordinator => RetainerBellCoordinatorLazy.Value;
 }
